ed85230acb800bcc14a8a9cbd42842e139723c8d
Teofilo Crisostomo <tcrisostomo@umadev.pe>
2024-11-27 04:14:12 +0000

    feat: se añade aplicación para manejar las aprobaciones.

---
+++ supply-approval/webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["pe/umadev/supplyapproval/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
---
+++ supply-approval/webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"peumadev/supply-approval/test/unit/controller/Principal.controller"
+], function () {
+	"use strict";
+});
